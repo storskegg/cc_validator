842151b5e0b2126c22793c9b6d29e83e13f5f219
Liam Conrad <liam@w4pho.net>
2025-07-04 16:08:21 -0500

    working

---
+++ CMakeFiles/cc_validator.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cc_validator.dir/main.cpp.o"
+  "CMakeFiles/cc_validator.dir/main.cpp.o.d"
+  "CMakeFiles/cc_validator.dir/validator.cpp.o"
+  "CMakeFiles/cc_validator.dir/validator.cpp.o.d"
+  "cc_validator"
+  "cc_validator.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cc_validator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CMakeFiles/cc_validator.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for cc_validator.
+# This may be replaced when dependencies are built.
