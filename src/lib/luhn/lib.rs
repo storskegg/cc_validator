e8b733e6933d14f7d346e6536be70a85ef7dd79e
Liam Conrad <liam@w4pho.net>
2025-07-21 16:41:38 -0500

    comments, fixed a bug

--- src/lib/luhn/lib.rs
+++ src/lib/luhn/lib.rs
@@ -7,9 +7,21 @@ pub enum LuhnResult {
     ErrBadInput,
 }
 
+// MAX_DATA is the maximum number of characters in the input string that we will handle
 pub const MAX_DATA: usize = 64;
 
-// TODO: Return Result<LuhnResult, ErrType>
+// ErrorBadInput is a custom error type for when we come across unsupported characters
+pub struct ErrorBadInput;
+
+impl std::fmt::Display for ErrorBadInput {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "Bad input: numerical string contains unsupported characters")
+    }
+}
+
+
+// validate performs the complete Luhn validation process. Unlike the C++ implementation in
+// this codebase, we handle this as a simple function, versus an instantiatable class.
 pub fn validate(data: &str) -> Result<LuhnResult, ErrorBadInput> {
     let mut local_data: String = data.to_string();
     local_data.truncate(MAX_DATA);
@@ -26,6 +38,7 @@ pub fn validate(data: &str) -> Result<LuhnResult, ErrorBadInput> {
 
     for (i, digit) in intermediary.digits.iter().enumerate() {
         sum += sum_digits(digit * current_multiplier);
+        current_multiplier = toggle_multiplier(current_multiplier);
     }
 
     let lm = (10 - (sum % 10)) % 10;
@@ -34,14 +47,6 @@ pub fn validate(data: &str) -> Result<LuhnResult, ErrorBadInput> {
     }
 
     Ok(LuhnResult::Valid)
-}
-
-pub struct ErrorBadInput;
-
-impl std::fmt::Display for ErrorBadInput {
-    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        write!(f, "Bad input: numerical string contains unsupported characters")
-    }
 }
 
 fn get_parity_digit(data: &str) -> Result<i32, ErrorBadInput> {
@@ -53,15 +58,16 @@ fn get_parity_digit(data: &str) -> Result<i32, ErrorBadInput> {
     }
 }
 
+// massage_input_string trims the input string, truncates it to MAX_DATA, and returns the result as
+// a string.
 fn massage_input_string(data: &str) -> String {
     data.to_string().trim().get(..MAX_DATA).unwrap().to_string()
 }
 
-/**
- * LuhnIntermediary is a struct that holds the numerical data and parity digit of the input string.
- * The digits are stored in reverse order, allowing for conventional iteration when the summing
- * process begins.
- */
+
+// LuhnIntermediary is a private struct that holds the numerical data and parity digit of the input
+// string. The digits are stored in reverse order, allowing for conventional iteration when the
+// summing process begins.
 struct LuhnIntermediary {
     parity_digit: i32,
     digits: Vec<i32>,
@@ -105,6 +111,7 @@ impl LuhnIntermediary {
 ////////////////////////////////////////////////
 
 
+// sum_digits calculates the sum of the digits in a given number. (e.g. 13 = 4)
 fn sum_digits(n: i32) -> i32 {
     let mut sum = 0;
     let mut n = n;
@@ -115,6 +122,7 @@ fn sum_digits(n: i32) -> i32 {
     sum
 }
 
+// toggle_multiplier toggles the multiplier for the next digit.
 fn toggle_multiplier(n: i32) -> i32 {
     match n {
         2 => 1,
