1d4a98410f4669097361c8938708a37e45e45491
Liam Conrad <liam@w4pho.net>
2025-07-23 16:57:43 -0500

    still playing, leaning dynamic widths and printing tuples

--- src/bin/gen_luhn_numbers/main.rs
+++ src/bin/gen_luhn_numbers/main.rs
@@ -4,23 +4,12 @@ const QTY_NUMBERS: u64 = 50;
 fn main() {
     let first_digit: u64 = 4;
 
-    // TODO: break the block into a function for testability and readability
-    let mult: u64 = {
-        // keep mutability localized within the scope of the block.
-        let mut result: u64 = 1;
-        for _ in 0..(NUM_DIGITS - 2) {
-            result *= 10;
-        }
-        result
-    };
-
+    let mult: u64 = derive_multiplier();
     let start_number: u64 = first_digit * (mult);
     if QTY_NUMBERS > (u64::MAX - start_number) {
         eprintln!("Error: QTY_NUMBERS is too large. QTY_NUMBERS {} Start {} Last {} Max_Supported {}", QTY_NUMBERS, start_number, u128::from(start_number+QTY_NUMBERS), u64::MAX);
         return;
     }
-
-    println!("Start = '{: >width$}'", start_number, width = usize::from(NUM_DIGITS));
 
     let mut last_number: u64 = 0;
     for i in 0..QTY_NUMBERS {
@@ -29,9 +18,26 @@ fn main() {
         let modulus: u32 = luhn::calculate_modulus_with_u64(from).cast_unsigned();
         let num: u64 = (from * 10) + u64::from(modulus);
         // println!("num   = '{: >width$}'", num, width = usize::from(NUM_DIGITS));
-        if i > 0 {
-            println!("delta = '{: >width$}'", num - last_number, width = usize::from(NUM_DIGITS));
-        }
+        let delta: u64 = calculate_delta(last_number, num);
+        // println!("delta = '{: >width$}'", delta, width = usize::from(NUM_DIGITS));
+        println!("(num, delta) = '{: >width$?}'", (num, delta), width = usize::from(NUM_DIGITS));
+
         last_number = num;
     }
+}
+
+fn calculate_delta(last: u64, current: u64) -> u64 {
+    if last == 0 {
+        return last;
+    }
+
+    current - last
+}
+
+fn derive_multiplier() -> u64 {
+    let mut result: u64 = 1;
+    for _ in 0..(NUM_DIGITS - 2) {
+        result *= 10;
+    }
+    result
 }
