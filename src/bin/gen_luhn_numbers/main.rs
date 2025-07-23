0519f50a698a0b415c5e0aa206271be90cc9dc0a
Liam Conrad <liam@w4pho.net>
2025-07-23 16:41:30 -0500

    safety dance, improved dynamic widths

--- src/bin/gen_luhn_numbers/main.rs
+++ src/bin/gen_luhn_numbers/main.rs
@@ -1,4 +1,4 @@
-const NUM_DIGITS: u16 = 10;
+const NUM_DIGITS: u16 = 5;
 const QTY_NUMBERS: u64 = 50;
 
 fn main() {
@@ -15,14 +15,18 @@ fn main() {
     };
 
     let start_number: u64 = first_digit * (mult);
+    if QTY_NUMBERS > (u64::MAX - start_number) {
+        eprintln!("Error: QTY_NUMBERS is too large. QTY_NUMBERS {} Start {} Last {} Max_Supported {}", QTY_NUMBERS, start_number, u128::from(start_number+QTY_NUMBERS), u64::MAX);
+        return;
+    }
 
-    println!("Start   = '{: >16}'", start_number);
+    println!("Start = '{: >width$}'", start_number, width = usize::from(NUM_DIGITS));
 
     for i in 0..QTY_NUMBERS {
         // learned dynamic padding here: https://stackoverflow.com/questions/69067436/how-do-i-make-the-fill-padding-in-stdformat-dynamic
         let from: u64 = u64::from(start_number + i);
         let modulus: u32 = luhn::calculate_modulus_with_u64(from).cast_unsigned();
         let num: u64 = (from * 10) + u64::from(modulus);
-        println!("num = '{: ^width$}'", num, width = usize::from(NUM_DIGITS));
+        println!("num   = '{: >width$}'", num, width = usize::from(NUM_DIGITS));
     }
 }
